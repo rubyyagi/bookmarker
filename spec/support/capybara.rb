ae655b01823b94080ef8bc76bc79c92db5cb80cc
Axel <axelshaded@gmail.com>
2021-01-20 22:23:50 +0800

    Added capybara support file
    Added capybara support file

---
+++ spec/support/capybara.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "selenium/webdriver"
+require "webdrivers/chromedriver"
+
+Capybara.server = :puma, { Silent: true }
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w(no-sandbox headless disable-gpu window-size=1280,800),
+    },
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome

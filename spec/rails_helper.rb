a30497a98d89b1ce9c94b6b277e1d472b8ccdb7c
Axel <axelshaded@gmail.com>
2021-01-20 23:10:30 +0800

    move config to rails helper
    move config to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,44 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+# frozen_string_literal: true
+
+require "selenium/webdriver"
+require "webdrivers/chromedriver"
+
+Capybara.server = :puma, { Silent: true }
+
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w(no-sandbox headless disable-gpu window-size=1280,800 remote-debugging-port=9222 disable-dev-shm-usage),
+    },
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
+
+# Setup rspec
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :chrome_headless
+  end
+end
+
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
